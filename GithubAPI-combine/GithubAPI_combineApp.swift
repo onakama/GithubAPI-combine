7ae7cd24d5d62d9ffd7b63f684bc581fdf5ca905
sumi <ninzin60en.dev@gmail.com>
2021-06-16 18:33:06 +0900

    Initial Commit

---
+++ GithubAPI-combine/GithubAPI_combineApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubAPI_combineApp.swift
+//  GithubAPI-combine
+//
+//  Created by nekuro on 2021/06/16.
+//
+
+import SwiftUI
+
+@main
+struct GithubAPI_combineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GithubAPI-combineTests/GithubAPI_combineTests.swift
@@ -0,0 +1,33 @@
+//
+//  GithubAPI_combineTests.swift
+//  GithubAPI-combineTests
+//
+//  Created by nekuro on 2021/06/16.
+//
+
+import XCTest
+@testable import GithubAPI_combine
+
+class GithubAPI_combineTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
